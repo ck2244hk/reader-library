fc446211912212e643e537ab98ab66ebd5e139f0
ck2244hk <ckleung.projs@gmail.com>
2026-06-04 23:22:42 -0400

    base debug 2

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // Set base to your GitHub repo name, e.g. '/reader-library/'
 // Change this to match your actual repo name before deploying
-// const base = process.env.VITE_BASE_PATH || 
+const base = process.env.VITE_BASE_PATH || '/'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/reader-library/',
+  base,
 })
